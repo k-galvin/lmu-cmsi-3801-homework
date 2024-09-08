2760949618df9fdb97919a1b35db99b4d17397c4
k-galvin <110426866+k-galvin@users.noreply.github.com>
2024-09-08 15:43:46 -0700

    Update lua styling and README

--- README.md
+++ README.md
@@ -3,3 +3,89 @@
 ## Kate Galvin
 
 ### Homework 1: Scripting
+
+In this assignment, I coded in Lua for the first time and got more practice with Python and JavaScript. This homework explored many topics including file processing, modules, and generators.
+
+## Testing Instructions
+
+The test files are included in the repo already. They are available for YOU! They will help you not only learn the languages and concepts covered in this course, but to help you with professional practice. You should get accustomed to writing code to make tests pass. As you grow in your profession, you will get used to writing your tests early.
+
+The test suites are run like so (assuming you have a Unix-like shell, modify as necessary if you have Windows):
+
+### Lua
+
+```
+lua exercises_test.lua
+```
+
+### Python
+
+```
+python3 exercises_test.py
+```
+
+### JavaScript
+
+```
+npm test
+```
+
+### Java
+
+```
+javac *.java && java ExercisesTest
+```
+
+### Kotlin
+
+```
+kotlinc *.kt -include-runtime -d test.jar && java -jar test.jar
+```
+
+### Swift
+
+```
+swiftc -o main exercises.swift main.swift && ./main
+```
+
+### TypeScript
+
+```
+npm test
+```
+
+### OCaml
+
+```
+ocamlc exercises.ml exercises_test.ml && ./a.out
+```
+
+### Haskell
+
+```
+ghc ExercisesTest.hs && ./ExercisesTest
+```
+
+### C
+
+```
+gcc string_stack.c string_stack_test.c && ./a.out
+```
+
+### C++
+
+```
+g++ -std=c++20 stack_test.cpp && ./a.out
+```
+
+### Rust
+
+```
+cargo test
+```
+
+### Go
+
+```
+go run restaurant.go
+```
--- lua/exercises.lua
+++ lua/exercises.lua
@@ -13,15 +13,18 @@ function change(amount)
   return counts
 end
 
--- Write your first then lower case function here
+-- Returns the lowercased first string in a that matches the predicate p
 function first_then_lower_case(a, p)
   for _, str in ipairs(a) do
-    if p(str) then return str:lower() end
+    if p(str) then
+      return str:lower()
+    end
   end
+  
   return nil
 end
 
--- Write your powers generator here
+-- Generator for powers of a base up to a given limit
 function powers_generator(base, limit)
   local exponent = 0
   local current_power = 1
@@ -30,15 +33,15 @@ function powers_generator(base, limit)
     while current_power <= limit do
       coroutine.yield(current_power)
       exponent = exponent + 1
-      current_power = base^exponent
+      current_power = base ^ exponent
     end
   end)
 end
 
--- Write your say function here
+-- Returns a string built from chained calls
 function say(word)
   local words = {}
-
+  
   local function add_string(input)
     if input == nil then
       return table.concat(words, " ")
@@ -47,59 +50,60 @@ function say(word)
       return add_string
     end
   end
-
+  
   if word ~= nil then
     table.insert(words, word)
     return add_string
   end
-
+  
   return ""
 end
 
--- Write your line count function here
+-- Counts the number of meaningful lines in a file
 function meaningful_line_count(filename)
   local file, err = io.open(filename, "r")
   if not file then
     error(err)
   end
-
+  
   local count = 0
-
+  
   for line in file:lines() do
-    local trimmed_line = string.gsub(line, "%s+", "")
-
+    local trimmed_line = line:gsub("%s+", "")
+    
     if trimmed_line ~= "" and not trimmed_line:match("^#") then
       count = count + 1
     end
   end
-
+  
   file:close()
-
+  
   return count
 end
 
--- Write your Quaternion table here
-Quaternion = (function()
-  local class = {}
-  local meta = {}
-  local prototype = {}
-
-  class.new = function (a, b, c, d)
+-- Quaternion class
+Quaternion = (function(class, meta, prototype)
+  -- Constructor for Quaternion
+  class.new = function(a, b, c, d)
     return setmetatable({a = a, b = b, c = c, d = d}, meta)
   end
-
+  
+  -- Returns conjugate of the Quaternion
   prototype.conjugate = function(self)
     return class.new(self.a, -self.b, -self.c, -self.d)
   end
-
+  
+  -- Returns the coefficients of the Quaternion
   prototype.coefficients = function(self)
     return {self.a, self.b, self.c, self.d}
   end
-
+  
+  -- Adds two Quaternions
   meta.__add = function(self, other)
     return class.new(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d)
   end
-
+  
+  -- Multiplies two Quaternions
   meta.__mul = function(self, other)
     return class.new(
       self.a * other.a - self.b * other.b - self.c * other.c - self.d * other.d,
@@ -108,54 +112,44 @@ Quaternion = (function()
       self.a * other.d + self.b * other.c - self.c * other.b + self.d * other.a
     )
   end
-
+  
+  -- Returns whether two Quaternions are equal
   meta.__eq = function(self, other)
     return(self.a == other.a and self.b == other.b and self.c == other.c and self.d == other.d)
-  end 
-
+  end
+  
+  -- Formats a Quaternion as a string
   meta.__tostring = function(self)
     local function process_coefficients(coefficient, letter)
       if coefficient == 0 then
-          return ""
+        return ""
       elseif coefficient == 1 then
-          if letter == "" then
-            return tostring(coefficient)
-          else
-            return ("+" .. letter)  
-          end
+        return letter == "" and "1.0" or "+" .. letter
       elseif coefficient == -1 then
-        if letter == "" then
-          return tostring(coefficient)
-        else
-          return ("-" .. letter)  
-        end
+        return letter == "" and "-1.0" or "-" .. letter
       elseif coefficient > 0 then
-          return "+" .. coefficient .. letter
+        return "+" .. coefficient .. letter
       else
-          return coefficient .. letter
+        return coefficient .. letter
       end
     end
-
+    
     local parts = {
-      process_coefficients(self.a, ""), 
-      process_coefficients(self.b, "i"), 
-      process_coefficients(self.c, "j"), 
+      process_coefficients(self.a, ""),
+      process_coefficients(self.b, "i"),
+      process_coefficients(self.c, "j"),
       process_coefficients(self.d, "k")
     }
-  
-    local result = table.concat(parts, "") 
-    if string.sub(result, 1, 1) == "+" then
-      result = string.sub(result, 2)
+    
+    local result = table.concat(parts, "")
+    if result:sub(1, 1) == "+" then
+      result = result:sub(2)
     end
-
-    if result == "" then
-      return "0"
-    else 
-      return result
-    end
+    
+    return result == "" and "0" or result
   end
-
+  
   meta.__index = prototype
-
+  
   return class
 end)({}, {}, {})
