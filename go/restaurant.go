afec8f2420e2dc52392b9427492f838c15592525
k-galvin <110426866+k-galvin@users.noreply.github.com>
2024-12-03 18:08:11 -0800

    Clean up go code

--- go/restaurant.go
+++ go/restaurant.go
@@ -8,11 +8,6 @@ import (
 	"time"
 )
 
-// A little utility that simulates performing a task for a random duration.
-// For example, calling do(10, "Remy", "is cooking") will compute a random
-// number of milliseconds between 5000 and 10000, log "Remy is cooking",
-// and sleep the current goroutine for that much time.
-
 func do(seconds int, action ...any) {
     log.Println(action...)
     randomMillis := 500 * seconds + rand.Intn(500 * seconds)
